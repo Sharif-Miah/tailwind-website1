1b5142096498a3ca6b3b2cb5f2f6954d138a6fba
farhan sharif <farhansharif1410@gmail.com>
2022-08-18 20:14:39 -0230

    some code added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// document.getElementsByClassName('shop-now').addEventListener('click', function () {
+//     console.log('This is shop now');
+// })
+
+const buttons = document.querySelectorAll('.buttons');
+
+for (const button of buttons) {
+    console.log(button);
+}
+
